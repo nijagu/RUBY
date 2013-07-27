20c13474d3d95e2620ddf6a4c5a95cd729c33719
Nick Gudowski <nick@ondeq.com>
2013-07-26 20:52:34 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyweblog::Application.config.secret_token = '136a93b5fdb7df060bcceea719b4bfdf07486037708b79287dcb70a2ff0c9e5ceba2890bef053de988e38a15824a28a29abfa80fc51d1d1ff9e6f6e325aca617'
